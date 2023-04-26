cf863e25bc7b8f187a374bf25d220af80aa02ca3
leebri101 <>
2023-04-26 14:51:24 +0000

    Item Class added

---
+++ classes/inventory.py
@@ -0,0 +1,8 @@
+# item class
+
+class Item:
+    def __init__(self, name, type, description, prop):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.prop = prop
