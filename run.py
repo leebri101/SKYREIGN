dcf258bf9cd0327ae2dac6a02bed6314d67216f3
leebri101 <>
2023-04-28 17:35:14 +0000

    Changes of choices menu

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from classes.game import person, bcolors
 from classes.magic import spell
 
 # Black Magic
-fire = spell("Fire", "MP:", 10,   100)
-thunder = spell("Thunder", "cost:", 15, 150)
-blizzard = spell("Blizzard", "cost:", 5, 50)
-meteor = spell("Fire", "cost:", 20, 200,)
+fire = spell("Fire:", "MP:", 10, 100)
+thunder = spell("Thunder:",  "MP:", 15, 150)
+blizzard = spell("Blizzard:", "MP:", 5, 50)
+meteor = spell("Fire:", "MP:", 20, 200,)
+
 
 # White Magic
-cure = spell("cure", 5, 50, "white")
-cura = spell("cura", 10, 100, "white")
-curaga = spell("curaga", 15, 150, "white")
+cure = spell("Cure:", "MP:", 5, 50)
+cura = spell("Cura:", "MP:", 10, 100)
+curaga = spell("Curaga:", "MP:", 15, 150)
 
 
 # Python code goes here
@@ -23,9 +24,9 @@ i = 0
 print(bcolors.FAIL + bcolors.BOLD + "Enemy Attack!!!" + bcolors.ENDC)
 
 while running:
-    print("======================")
+    print(bcolors.WHITE + bcolors.BOLD + "==================================")
     player.choose_action()
-    choice = input("Choose Action:")
+    choice = input(bcolors.CYAN + "Choose Action:")
     index = int(choice) - 1
 
     if index == 0:
@@ -34,16 +35,18 @@ while running:
         print("You attacked", dmg, "Points of DMG.")
     elif index == 1:
         player.choose_magic()
-        magic_choice = int(input("Choose Magic:")) - 1
+        magic_choice = int(input()) - 1
 
         spell = player.magic[magic_choice]
         magic_dmg = spell.generate_damage()
 
         current_mp = player.get_mp()
 
-        if spell.cost > current_mp:
-            print(bcolors.FAIL + "\nNot Enough MP\n" + bcolors.ENDC)
-            continue
+        current_mp = input(spell.cost)
+        if int(spell.cost) > current_mp:
+            print("Please Choose a spell")
+        else:
+            print("Not Enough MP") 
        
         player.reduce_mp(cost)
         enemy.take_damage(magic_dmg)
@@ -57,13 +60,13 @@ while running:
     print("Enemy Attacked", enemy_dmg)
 
 
-    print("======================")
-    print("Player HP:", bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + bcolors.ENDC + "\n")
+    print(bcolors.WHITE + bcolors.BOLD + "==================================")
+    print("")
+    print("Player HP:", bcolors.OKGREEN + bcolors.BOLD + str(player.get_hp()) + "/" + str(player.get_max_hp()) + bcolors.ENDC + "\n")
     
-    print("Player MP:", bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + bcolors.ENDC + "\n")
+    print(bcolors.WHITE + bcolors.BOLD + "Player MP:", bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + bcolors.ENDC + "\n")
     
-    print("Enemy HP:", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC + "\n")
-
+    print(bcolors.WHITE + bcolors.BOLD + "Enemy HP:", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC + "\n")
 
     if enemy.get_hp() == 0:
         print(bcolors.OKGREEN + "You Won!" + bcolors.ENDC)
