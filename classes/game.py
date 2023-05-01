c0f58be33845620f244bc6cd8664e3439636edea
leebri101 <>
2023-05-01 13:39:19 +0000

    Readjusted Length of code

--- classes/game.py
+++ classes/game.py
@@ -1,6 +1,6 @@
 import random
 import pprint
-from classes.magic import spell
+from class .magic import spell
 
 class bcolors:
     HEADER = '\033[95m'
@@ -8,13 +8,13 @@ class bcolors:
     WARNING = '\033[93m'
     OKGREEN = '\033[92m'
     FAIL = '\033[91m'
-    WHITE ='\033[37m'
+    WHITE = '\033[37m'
     CYAN = '\033[36m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-class person:
+class player:
     def __init__(self, hp, mp, atk, df, magic):
         self.maxhp = hp
         self.hp = hp
@@ -63,7 +63,7 @@ class person:
 
     def choose_action(self):
         i = 1
-        print(bcolors.OKGREEN + bcolors.BOLD + "Please Pick An Action:" + bcolors.ENDC)
+        print(bcolors.OKGREEN + bcolors.BOLD + "Pick An Action:" + bcolors.ENDC)
         for item in self.actions:
             print(str(i) + ":", item)
             i += 1
